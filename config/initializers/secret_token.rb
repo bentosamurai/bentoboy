53a6293132e45985f707531a9287f525f7fb936f
Tsubasa Suda <speakerboxxer@gmail.com>
2011-12-08 23:50:18 +0900

    start bentoboy!!!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bentoboy::Application.config.secret_token = 'f360ce08b1abbcf37f0983cc06dd87eeec0f03987a228d37eeb9191b78bd476239023eb3b4c9397c97a08e4b34b5588c319973a96a977dd58a5da46910d85de1'
